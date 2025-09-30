466b3d0be4b59609611fed9145728e9b339b6645
inijamlud <jamalude449@gmail.com>
2025-10-01 05:54:49 +0700

    Belajar Golang | 7. Map

---
+++ map/main.go
@@ -0,0 +1,25 @@
+package main
+
+func main() {
+	// mahasiswa := map[string]string{
+	// 	"nama":  "mal",
+	// 	"kelas": "A",
+	// }
+
+	// fmt.Println(mahasiswa)
+	// fmt.Println("nama:", mahasiswa["nama"])
+
+	// mahasiswa["jurusan"] = "informatika"
+	// fmt.Println("jurusan:", mahasiswa["jurusan"])
+
+	// // nama, kelas, jurusan
+	// fmt.Println(mahasiswa)
+
+	// ========================================
+	// mahasiswa := map[string]int{
+	// 	"mal":  89,
+	// 	"joko": 80,
+	// 	"ara":  67,
+	// }
+
+}
